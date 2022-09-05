7e8058ee4b0d94b998ae0c9dbc9a98bd8bd106e9
Niloy Julious <niloyjulious@gmail.com>
2022-09-05 22:13:48 +0600

    Add files via upload

---
+++ config.py
@@ -0,0 +1,23 @@
+telegram_api_id = '7129805'
+telegram_api_hash = '23f6c756682f7e26427d4462f9075030'
+telethon_session_string = '1BVtsOLQnu0BBhpom4k8PofHIHyPZGMD_3b0gI54Yi2tMy1UjwLpF7y0AoZGY2yBSl3u1SAG42LNCt8DjylPKRBnR2QIuUuhSXGRhDBz1Vh6uUKTtF5WGYt-k207JwhyYPx9i6LWKQVaOAXDR2l_Eboah8qQokoLSs0b-2grTRKUHG8d3Z5kbZL2plU2ka7Ku0rCzis1FS8xf8XN03GNlA9RyWK7TtUKgIl9ME2WebhtR_xk5vzQnvnZby9YiyAH2E0cOQAftq6-_V3YtzC_hapFtuK9XL2JK-v1XWKm1NEVq1BCR-XB9Sh1Qkt0RiraUwfDfZajUporaMSDKj1oFVgYTmdqcJR0='
+
+
+google_credentials = {
+    "type": "service_account",
+    "project_id": "sheets-api-336180",
+    "private_key_id": "5b3900364bd472026d3b2c830801f1924818e578",
+    "private_key": "-----BEGIN PRIVATE KEY-----\nMIIn8QIBADANcgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQChGcnqGhnL9xf9\n0HoUqJYN898cDYcsck+ShDBcK+P43NCQ3KiRDskDxDPCX/clkWVEoMBg0rMlNXvf\nKszZVwnUv+6v04Li5woq08c6JAgU/Cu64/KT8oOgw1IO6x4blOG6TheYbsXJ7NBS\nZk+Hq3L4m8I0pENHgrScOMXKiLQoJj0FYsnc+EAo1ilTqL4alty+aBD4ajSpXTgZ\nPPBIOYF3hW09rvlz+jf2PJGcZmsz9cA7NnxRBknW5dS4PB/Zksyp+AzV+1lyhkRS\nURtWmt3BKcTxH1YXuypuulTy+7tHYVem8u/QcCUzMhjyiOOoC07Qi5h1kaLkf8qw\nE3ydvrM9AgMBAAECggEADtXEHTWoGHs5j1rqUwqvPrutWPT59KLWJwTbOzGjfaA/\n6IcZUrpM0nb32TdjF+eKrsYq0P+2FYAWVxem44C7XAcMoM5WaDSkYAs+dmEpCV2r\nXTtudCdJ7bzuyobmL94G51QXoibZOd3/1Qas3i5wmOom+RgDRguIPDaSKDNse6Hc\nYGeJEixJ3mTFS/1aUqgmhUP6wiOw6lvBV/C/x3ijIci3nW2MIStqwFfEZd3qxEIr\n7L97Afm2vjiFwCKBweUN+Mpd0FpWEuDunI7Ap+qndvrqNHJpIJnNLx1raJcjesm6\nh0WawwBB7b37rChkl4XNH9qSOCrHkljPHDy/28RNAQKBgQDTLW3RVetNw/66WFDv\nyWf/Y6a8V2qmS014+9LjfarzP1xMxId8WY37Z29mWFPmkJebLypL0ENXRtJMi0sS\nS28eZCaED/Tc+EN4d7yiC2tqKMBbVVkWuQ9/7trme8J0DHJJfM2okdczQMua7DNU\nZf5KPVLjJ/PlYufYyPbJ9AzYQQKBgQDDS2IjQY15rqqRLo7PdkehRXCFXRDnO8z9\nx++DQj8JTVWQtVcaEPuIM5P/t4YrIEuD0KS9ReU782ktdJOmgH0Fc2ehWJ6rFlIs\ndstly32HdouNWkDFtgPHpSI4Ngtri080zvsnkvfsurUpj+WgOrIWBiXp1EwYJpaR\ncIOPebY7/QKBgE1tga9YPk8kBpiSWxKgGQZ1piO2NX9FauFqC5encu9/Ert+72Iz\nf7Bm61xuFE0cRMn8UBi+AoLUj7TK+bp6eAJ/m/Od2hzO1gYseLg9UaP5zxVtxJ1l\nOv6gOBh71f8wtRKmtM0StAGxxzVFV0ihoMvdki+BbZZ0XieI/5tqZO9BAoGBAJKJ\ngta/fj96ryOG0sOalb7N0tNO9Lr85SDbn71WvOPm8jhFyViWu5YEVZ3AAF1luyPq\nL2KSCqKETzw7R/rcWHL5mB252Dy53+jbLl1wZf16FtZ2jTmNDKMGJnoVG1aTzlfq\nowubvIoVwo+wGHW09SGPdW1RzdMlST9SNHU4t2OlAoGAO+T8RJsBBC/TWmOw4Ymx\nDS1X+z9Jsm9Ndp6hYguA0uxQuNIJsUYOr/PXnW43hysMLABX3BY98bvnqBNN62Nb\nABaOuLjTwd5b6ya4PTMC03vneI7+q1bgX49U/QoRAccE8FuFous9DrR36PdfehHP\nOhbwTMWLPGbiT3hDfq/ROg8=\n-----END PRIVATE KEY-----\n",
+    "client_email": "salman@sheets-api-336180.iam.gserviceaccount.com",
+    "client_id": "113569598832184559698",
+    "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+    "token_uri": "https://oauth2.googleapis.com/token",
+    "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+    "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/salman%40sheets-api-336180.iam.gserviceaccount.com"
+}
+
+sheets_dictionary = {
+    'PriceAction Forex VIP': '1mI6WJA8XycZFcnvRUnh79Nf-xIsdF8SuT0IsQk173Ts',
+    'PriceAction ICC VIP': '1BLHoDKwNxFrnXf8RBvb728aM7SjLnVWS0qasMUP0vT0',
+    'SCALP WITH PAFX VIP': '16TXtWuGK9IvuvR93PYQn17tQFmw7m2zNIWVglkTvsww'
+}
